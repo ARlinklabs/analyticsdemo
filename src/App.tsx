c092ef9eb5eb1ff372006debd522c2afa7013129
internet_trash <naiknischal92@gmail.com>
2024-10-04 10:09:21 +0530

    osuh

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { AnalyticsDashboardComponent } from './components/analytics-dashboard';
-import AnalyticsWrapper from './scripts/AnalyticsWrapper';
+//import AnalyticsWrapper from './scripts/AnalyticsWrapper';
+import  AnalyticsWrapper  from 'arlink-analytics/src/AnalyticsWrapper';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
--- src/components/analytics-dashboard.tsx
+++ src/components/analytics-dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { dryrun } from "@permaweb/aoconnect";
